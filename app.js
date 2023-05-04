727f1aa68e61f89c58befb514cd7f5cfc68c719c
Arantxa Martinez <Shayne.martinez23@gmail.com>
2023-05-04 01:03:55 -0400

    Styling

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose')
-var session = require('express-session')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var session = require("express-session");
 
-var MongoStore = require('connect-mongo')
+var MongoStore = require("connect-mongo");
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth')
-var productRouter = require('./routes/product')
-var reviewsRouter = require('./routes/reviews')
-var homeRouter = require('./routes/home');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
+var productRouter = require("./routes/product");
+var reviewsRouter = require("./routes/reviews");
+var homeRouter = require("./routes/home");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 // use session
 app.use(
@@ -33,58 +30,56 @@ app.use(
     resave: true,
     saveUninitialized: false,
     cookie: {
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      maxAge: 6000000 // 60 * 1000 ms === 1 min
+      maxAge: 6000000, // 60 * 1000 ms === 1 min
     }, // ADDED code below !!!
     store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI
+      mongoUrl: process.env.MONGODB_URI,
 
       // ttl => time to live
       // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
-    })
+    }),
   })
 );
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/auth', authRouter);
-app.use('/product', productRouter);
-app.use('/reviews', reviewsRouter);
-app.use('/home', homeRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
+app.use("/product", productRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/home", homeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(`An error occurred while connecting to the Database: ${err}`);
   });
-  
 
 module.exports = app;
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('home', { title: 'Bliss Studio' });
+router.get("/", (req, res) => {
+  res.render("home", { title: "Bliss Studio" });
 });
 
 module.exports = router;
